f1c0b4c43961e6218d071addafa4e23254f56d5c
seaweed78 <adhikarikshitiz12@gmail.com>
2023-02-14 11:18:58 +0545

    assignment1

---
+++ assignment1/Kshitiz.java
@@ -0,0 +1,15 @@
+public class Kshitiz
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name of the organization: Mastermind ABC \n");
+        System.out.println("Address: ");
+        System.out.println("----State: KTM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal \n");
+        System.out.println("Contact Information: ");
+        System.out.println("----Mobile Number: 9808481423");
+        System.out.println("----Email: abc.def@email.com \n");
+
+    }
+}
---
+++ basic/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("what haha");
+        System.out.println("what haha");
+        System.out.print("what World");
+        System.out.print("Same line World");
+    }
+}
